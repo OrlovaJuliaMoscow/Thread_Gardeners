d52434c7715fed31bd2e5162fcb63ce4cb3d9939
orlova.julia.ivan@gmail.com <orlova.julia.ivan@gmail.com>
2023-03-05 00:00:49 +0300

    Добавьте файлы проекта.

---
+++ Thread_Gardeners/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Thread_Gardeners
+{
+    class Program
+    {
+        const int n = 10;
+        //const int m = 2;
+
+        static int[] path = new int[n] { 1, 2, 0, 20, 5, 0, 1, 2, 6, 10 };
+        static void Main(string[] args)
+        {
+            ThreadStart threadStart = new ThreadStart(Gardner1);
+            Thread thread = new Thread(threadStart);
+            thread.Start();
+
+            Gardner2();
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"{path[i]} ");
+            }
+
+            Console.ReadKey();
+        }
+
+
+
+
+        static void Gardner1()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (path[i] >= 0) 
+                {
+                    int delay = path[i];
+                    path[i] = -1;
+                    Thread.Sleep(delay);
+                }
+            }
+        }
+        static void Gardner2()
+        {
+            for (int i = n-1; i >=0; i--)
+            {
+                if (path[i] >= 0) 
+                {
+                    int delay = path[i];
+                    path[i] = -2;
+                    Thread.Sleep(delay);
+                }
+
+            }
+        }
+    }
+}
---
+++ Двумерный массив_Gardeners/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Thread_Gardeners
+{
+    class Program
+    {
+        const int n = 4;
+        //const int m = 2;
+
+        static int[,] path = new int[n, n];  //{1, 2, 0, 20, 5, 0, 1, 2, 6, 10, 1, 2, 0, 20, 5, 0, 1, 2, 6, 1};
+        static void Main(string[] args)
+        {
+            ThreadStart threadStart = new ThreadStart(Gardner1);
+            Thread thread = new Thread(threadStart);
+            thread.Start();
+
+            Gardner2();
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"{path[i,j]} ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+
+
+
+
+        static void Gardner1()
+        {
+            for (int i = 0; i < n; i++)
+            { 
+                for (int j = 0; j < n; j++)
+                {
+
+
+                    if (path[i,j] >= 0) //array[n,m] >= 0)
+                    {
+                        int delay = path[i, j];
+                        path[i, j] = -1;
+                        Thread.Sleep(delay);
+                    }
+                }
+            }
+        }
+        static void Gardner2()
+        {
+            for (int i = n - 1; i >= 0; i--)
+            {
+                for (int j = n - 1; j >= 0; j--)
+                {
+                    if (path[i, j] >= 0)
+                    {
+                        int delay = path[i, j];
+                        path[i, j] = -2;
+                        Thread.Sleep(delay);
+                    }
+                }
+            }
+        }
+    }
+}
